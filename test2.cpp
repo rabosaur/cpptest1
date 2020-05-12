05f2d4ee2f6fdf1a2da333896b7194815e9866af
rabosaur <git@coffeezombie.com>
2020-05-12 10:05:37 -0700

    new file for mirror test

---
+++ test2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(int argc, char **argv)
+{
+    string s1="Hello";
+    string s2="World";
+    cout << "HELLO!\n";
+    cout <<s1+" " + s2 << "\n";
+    cout <<s1+" " + s2 << "\n";
+    cout <<s1+" " + s2 << "\n";
+    return 0;
+}
+
